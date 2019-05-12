f44577d1451ba74585fde131419986d9b9685dd4
George W. Walker <george@georgewwalker.com>
2019-05-11 22:36:11 -0500

    Add delay

--- lirc.js
+++ lirc.js
@@ -8,18 +8,23 @@ module.exports = function Lirc(config) {
 	};
 
 	var sendKey = function(key, resolve, reject) {
-		var client = net.connect({host: config.host, port: config.port || 8765}, function() {
-			var command = 'SEND_ONCE ' + config.remote + ' ' + key;
-			console.log('connected to host about to issue command(s): ' + command);
-			config.test || client.write(command + '\r\n');
-			client.end();
-			setTimeout(resolve, config.delay || 250);
-		});
-
-		client.on('error', function(error) {
-			console.log("failed to communicate with host");
-			reject(error);
-		});
+		if (key.indexOf("DELAY") !== -1) {
+			var delayTimeout = key.indexOf("|") !== -1 ? parseInt(key.split("|")[1]) : config.delay || 250;
+			setTimeout(resolve, delayTimeout);
+		} else {
+			var client = net.connect({host: config.host, port: config.port || 8765}, function() {
+				var command = 'SEND_ONCE ' + config.remote + ' ' + key;
+				console.log('connected to host about to issue command(s): ' + command);
+				config.test || client.write(command + '\r\n');
+				client.end();
+				setTimeout(resolve, config.delay || 250);
+			});
+	
+			client.on('error', function(error) {
+				console.log("failed to communicate with host");
+				reject(error);
+			});
+		}
 	};
 
 	var sendKeys = function(keys) {
